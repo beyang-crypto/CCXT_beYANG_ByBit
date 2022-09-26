236e98b17a10b4dcc0e60daebe3561d3efc5e33f
TestingAccMar <114166594+TestingAccMar@users.noreply.github.com>
2022-09-26 22:58:43 +0400

    Delete bybit/v3 directory

--- bybit/v3/ws/request/requestBinance.go
+++
@@ -1,5 +0,0 @@
-package request
-
-import binanceWs "github.com/TestingAccMar/CCXT_beYANG_Binance/binance/ws"
-
-type BinanceBookTicker binanceWs.BookTicker
--- bybit/v3/ws/request/requestByBit.go
+++
@@ -1,7 +0,0 @@
-package request
-
-import (
-	bybit "github.com/TestingAccMar/CCXT_beYANG_ByBit/bybit/spot/v3"
-)
-
-type A bybit.BookTicker
