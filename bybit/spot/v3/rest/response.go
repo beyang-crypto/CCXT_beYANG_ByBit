331f03261ceddb3e8b63b9db182dc640de579023
TestingAccMar <114166594+TestingAccMar@users.noreply.github.com>
2022-09-27 23:02:39 +0400

    Add files via upload

---
+++ bybit/spot/v3/rest/response.go
@@ -0,0 +1,30 @@
+package rest
+
+// https://bybit-exchange.github.io/docs/spot/v3/#t-balance
+type WalletBalance struct {
+	RetCode    int                     `json:"retCode"`
+	RetMsg     string                  `json:"retMsg"`
+	Result     ResultWalletBalance     `json:"result"`
+	RetExtMap  RetExtMapWalletBalance  `json:"retExtMap"`
+	RetExtInfo RetExtInfoWalletBalance `json:"retExtInfo"`
+	Time       int64                   `json:"time"`
+}
+type BalancesWalletBalance struct {
+	Coin   string `json:"coin"`
+	CoinID string `json:"coinId"`
+	Total  string `json:"total"`
+	Free   string `json:"free"`
+	Locked string `json:"locked"`
+}
+type ResultWalletBalance struct {
+	Balances []BalancesWalletBalance `json:"balances"`
+}
+type RetExtMapWalletBalance struct {
+}
+type RetExtInfoWalletBalance struct {
+}
+
+func BybitToWalletBalance(data interface{}) WalletBalance {
+	bt, _ := data.(WalletBalance)
+	return bt
+}
